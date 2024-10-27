f2edcef89fde0a74b17986c3bf0bec9a6198311a
Falguni Mondal <career.falguni@gmail.com>
2024-10-27 14:15:16 +0530

    Fixed possible bugs in mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
 import Router from './utils/Router'
 import Navbar from './components/navbar/Navbar'
-import { useGSAP } from '@gsap/react'
+import { useEffect, useCallback } from 'react'
 import gsap from 'gsap'
 import Footer from './components/footer/Footer'
 
 const App = () => {
-  let mouseFollower;
-  useGSAP(
-    mouseFollower = (e) => {
-      gsap.to(".mouseFollower", {
-        x: e.clientX - 5,
-        y: e.clientY - 5,
-        opacity: 1,
-        duration: 0.2
-      })
+  const mouseFollower = useCallback((e) => {
+    gsap.to(".mouseFollower", {
+      x: e.clientX - 5,
+      y: e.clientY - 5,
+      opacity: 1,
+      duration: 0.2
+    })
+  }, [])
+
+  useEffect(() => {
+    const handleMouseMove = (e) => mouseFollower(e)
+    window.addEventListener('mousemove', handleMouseMove)
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
     }
-  )
+  }, [mouseFollower])
 
   return (
-    <div onMouseMove={(e) => mouseFollower(e)} className='w-screen h-screen relative overflow-hidden'>
+    <>
       <span className="mouseFollower hidden absolute lg:block opacity-0 w-[12px] h-[12px] rounded-full bg-[#3d3d3d] top-0 left-0 z-[100] pointer-events-none"></span>
-      <div className="outer-container w-full h-full overflow-y-auto flex flex-col items-center bg-[#fff]">
+      <div className="container w-full bg-[#fff]">
         <Navbar />
-        <div className='container w-full'>
-          <main>
-            <Router />
-          </main>
-          <Footer />
-        </div>
+        <main>
+          <Router />
+        </main>
+        <Footer />
       </div>
-    </div>
+    </>
   )
 }
 
