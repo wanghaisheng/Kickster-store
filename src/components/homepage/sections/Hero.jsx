c77704b917948c5a277972c7edae8e3354549458
falguni-mondal <career.falguni@gmail.com>
2024-11-06 08:24:58 +0530

    Downscale of the hero img

--- src/components/homepage/sections/Hero.jsx
+++ src/components/homepage/sections/Hero.jsx
@@ -7,14 +7,14 @@ const Hero = () => {
     useGSAP(() => {
         gsap.from(".main-heading-txt", {
             y: "60vh",
-            duration: 1,
+            duration: 1.2,
             delay: 0.2,
             stagger: 0.1,
             ease: "power1.out",
         })
         gsap.from(".shoeImg", {
             opacity: 0,
-            duration: 0.5,
+            duration: 0.6,
             delay: 1.5,
             ease: "power1.out",
         })
@@ -26,7 +26,7 @@ const Hero = () => {
                 <NavLink to="/shoes/sale"><span className="inline-block w-[5px] h-[5px] rounded-full bg-[#34ff22] shadow-[0px_0px_10px_1px_#20f70c] mb-1"></span> On sale</NavLink>
                 <p className='w-[25ch]'>Discover branded footwear to kickstart your wardrobe, one step at a time —</p>
             </div>
-            <div className="heading-container w-full h-fit overflow-hidden">
+            <div className="heading-container w-full h-fit overflow-hidden pb-[1.5vh]">
                 <div className="main-heading flex-shrink-0 w-full">
                     <h1 className='w-full h-fit bg-[#fff] mix-blend-lighten flex justify-center text-[6.8rem] lg:text-[22rem] 2xl:text-[25rem] leading-none tracking-[-5px] lg:tracking-[-10px]'>
                         {
@@ -36,7 +36,7 @@ const Hero = () => {
                         }
                     </h1>
                 </div>
-                <img className='shoeImg absolute bottom-[8vh] lg:bottom-[3vh] left-[50%] -translate-x-[50%] h-[30vh] lg:h-[95vh] max-h-[750px] z-10' src="../../../../assets/images/shoeNobgL.png" alt="Nike Shoe Img" />
+                <img className='shoeImg absolute bottom-[9vh] lg:bottom-[3vh] left-[50%] -translate-x-[50%] h-[30vh] lg:h-[95vh] max-h-[750px] z-10' src="../../../../assets/images/shoeNobgL-min.png" alt="Nike Shoe Img" fetchPriority='highest' />
             </div>
         </header>
     )
