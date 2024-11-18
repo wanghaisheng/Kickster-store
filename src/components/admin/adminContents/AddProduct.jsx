bd3722f55bc3190b1bdd2001dc7b115c11fdb23e
falguni-mondal <career.falguni@gmail.com>
2024-11-18 21:23:33 +0530

    Product adding feature included

--- src/components/admin/adminContents/AddProduct.jsx
+++ src/components/admin/adminContents/AddProduct.jsx
@@ -10,25 +10,37 @@ import {
   getFirestore,
   setDoc
 } from "firebase/firestore";
-import {
-  getDownloadURL,
-  getStorage,
-  ref as s_ref,
-  uploadBytes
-} from "firebase/storage";
+// import {
+//   getDownloadURL,
+//   getStorage,
+//   ref as s_ref,
+//   uploadBytes
+// } from "firebase/storage";
 import { uuid } from "../../../utils/uuid";
+import { useNavigate } from "react-router-dom";
 
 const AddProduct = () => {
   const [reveal, setReveal] = useState(false);
   const [productCategory, setProductCategory] = useState("Select");
   const [newProduct, setNewProduct] = useState(false);
-  const [ productImg, setProductImg ] = useState(null);
-  const { register, handleSubmit, reset } = useForm();
+  // const [ productImg, setProductImg ] = useState(null);
+  let productImg = [];
+  const [ imgReveal, setImgReveal ] = useState(false)
+  const { 
+    register,
+    handleSubmit,
+    reset,
+    formState: {error, isSubmitting},
+   } = useForm({
+      defaultValues: {
+        images: ["","","","",""]
+      }
+    });
   const productCategorySetter = (option) => {
     setProductCategory(option);
     setReveal(false);
   };
-  const [ id ] = useState(uuid());
+  const [ id, setId ] = useState(uuid());
 
   //! Firestore database initialization
   const db = getFirestore(app);
@@ -37,7 +49,22 @@ const AddProduct = () => {
   // Storage Initialization
   // const storage = getStorage(app);
 
+
+  const productsPageNavigator = () => {
+    const navigate = useNavigate();
+    navigate("/admin/products");
+  }
+
+  const priceCorrection = (orgPrice) => {
+    const price = parseInt(orgPrice);
+    const commaPos = parseInt(price / 1000).toString().length;
+    let priceArray = [...orgPrice];
+    priceArray.splice(commaPos, 0, ",");
+    return priceArray.join("");
+  }
+  // ! FORM HANDLER
   const formHandler = async(data) => {
+    const price = priceCorrection(data.price)
     // const productImgURL = [];
     
     // Uploading the data to the storage by the name of Data ID
@@ -51,20 +78,27 @@ const AddProduct = () => {
     // })
 
     //! Adding the data to Firestore Collection with the Data ID as the document ID
-    await setDoc(doc(colRef, `${data.id}`), {
+    await setDoc(doc(colRef, `${id}`), {
       id: id,
       title: data.title,
-      images: productImg,
+      images: [...data.images],
       category: productCategory,
       description: data.description,
-      stock: data.stock,
-      discount: data.discount,
+      stock: parseInt(data.stock),
+      discount: parseInt(data.discount),
       new: newProduct,
-      brand: data.brand
+      brand: data.brand,
+      price: price,
+      reviews: [],
+      sales: 0,
     })
     console.log("Success!");
+    setId(uuid());
+    setProductCategory("Select");
+    setNewProduct(false);
     reset();
   }
+
 
   return (
     <div className="w-full h-[84vh] bg-white flex justify-center py-5">
@@ -76,7 +110,7 @@ const AddProduct = () => {
         </h2>
 
         {/* ....................... FORM ........................ */}
-        <form onSubmit={handleSubmit(data => formHandler(data))} className="grid grid-cols-6 gap-[16px] mt-[24px]">
+        <form onSubmit={handleSubmit(formHandler)} className="grid grid-cols-6 gap-[16px] mt-[24px]">
 
           {/* Title */}
           <div className="input-container col-span-4">
@@ -88,7 +122,7 @@ const AddProduct = () => {
           <div className="input-container col-span-2">
             <label htmlFor="#product-price">Price</label>
             <div className="price-inp-container w-full relative overflow-hidden">
-              <input {...register("price")} className="w-full" required type="number" id="#product-price" />
+              <input {...register("price")} className="w-full" required type="text" id="#product-price" />
               <span className="rupee-icon w-[30px] h-full flex justify-center items-center absolute top-[50%] -translate-y-[50%] bg-zinc-200">
                 <LiaRupeeSignSolid />
               </span>
@@ -105,11 +139,11 @@ const AddProduct = () => {
           <div className="col-span-2">
             <label>Category</label>
             <div className="category relative w-full">
-              <span onClick={() => setReveal((prev) => !prev)} className="flex justify-between items-center w-full border border-zinc-400 rounded py-1 px-3">
+              <span onClick={() => setReveal((prev) => !prev)} className="flex justify-between items-center w-full border border-zinc-400  rounded py-1 px-3">
                 {productCategory}
                 <RiArrowDownSLine className="text-[1.2rem]" />
               </span>
-              <div className={`category-options ${!reveal && "hidden"} w-full border-2 border-zinc-200 bg-zinc-200 rounded absolute left-0 top-[40px]`}>
+              <div className={`category-options ${!reveal && "hidden"} w-full border-2 border-zinc-200 bg-[#dadada9c] backdrop-blur-md rounded absolute left-0 top-[40px]`}>
                 <span onClick={() => productCategorySetter("Men's Shoes")} className="block p-3 font-medium hover:bg-white duration-300 transition-all">
                   Men's Shoe
                 </span>
@@ -130,7 +164,7 @@ const AddProduct = () => {
           <div className="input-container col-span-2">
             <label htmlFor="#product-dis">Discount</label>
             <div className="dis-inp-container w-full relative overflow-hidden">
-              <input {...register("discount")} className="w-full" required type="number" id="#product-dis" />
+              <input {...register("discount")} className="w-full" required type="text" id="#product-dis" />
               <span className="rupee-icon w-[30px] h-full flex justify-center items-center absolute top-[50%] -translate-y-[50%] bg-zinc-200">
                 %
               </span>
@@ -140,7 +174,7 @@ const AddProduct = () => {
           {/* Product Stock */}
           <div className="input-container col-span-2">
             <label htmlFor="#product-stock">Stock</label>
-            <input {...register("stock")} required type="number" id="#product-stock" />
+            <input {...register("stock")} required type="text" id="#product-stock" />
           </div>
 
           {/* Product Images */}
@@ -151,7 +185,19 @@ const AddProduct = () => {
 
           <div className="col-span-4">
             <label>Images</label>
-            <input onChange={(e)=> setProductImg([...e.target.files])} className="image-inp w-full" required type="file" multiple />
+            <div className="productImg relative w-full">
+              <span onClick={() => setImgReveal((prev) => !prev)} className="flex justify-between items-center w-full border border-zinc-400 rounded py-1 px-3">
+                <span>Select Images</span>
+                <RiArrowDownSLine className="text-[1.2rem]" />
+              </span>
+              <div className={`image-url-inputs ${!imgReveal && "hidden"} w-full border-2 p-2 border-zinc-200 bg-[#dadada9c] backdrop-blur-md absolute left-0 top-[40px] flex flex-col gap-1 rounded`}>
+                {
+                  ["","","","",""].map( (item, index) => (
+                    <input {...register(`images.${index}`)} key={`product_image_${index}${item}`} className="product-img-inp placeholder:text-zinc-600 placeholder:text-[0.9rem]" type="url" placeholder={`${ index !== 4 ? `Image ${index + 1}` : "Model"}`} required />
+                  ))
+                }
+              </div>
+            </div>
           </div>
 
           {/* New Checkbox */}
@@ -169,8 +215,8 @@ const AddProduct = () => {
 
           {/* Product Buttons */}
           <div className="add-product-btns col-span-6">
-          <button className="w-[10vw] min-w-[150px] py-[5px] rounded border border-zinc-800 mr-[24px]" type="submit">Cancel</button>
-          <button className="w-[10vw] min-w-[150px] py-[5px] bg-zinc-800 rounded text-zinc-100" type="submit">Add Product</button>
+          <button onClick={()=> productsPageNavigator} className="w-[10vw] min-w-[150px] py-[5px] rounded border border-zinc-800 mr-[24px]" type="button">Cancel</button>
+          <button className="w-[10vw] min-w-[150px] py-[5px] bg-zinc-800 rounded text-zinc-100" type="submit" disabled={isSubmitting}>{isSubmitting ? "Adding..." : "Add Product"}</button>
           </div>
         </form>
       </div>
