fd5f3bff86bc7b1540fd1708c7e6252702322420
falguni-mondal <career.falguni@gmail.com>
2024-11-26 04:32:23 +0530

    solved data missing issue on product load

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -4,7 +4,7 @@ import ProductDescription from './sections/productDescription/ProductDescription
 import ProductCards from '../universal/ProductCards'
 import { useParams } from 'react-router-dom'
 import { collection, doc, getDoc, getFirestore } from 'firebase/firestore'
-import app from '../../utils/firebaseConfigures'
+import app, { db } from '../../utils/firebaseConfigures'
 import Loader from '../loader/Loader'
 
 const Product = () => {
@@ -14,8 +14,6 @@ const Product = () => {
 
 
   const productFetcher = async (productId) => {
-    // Firestore database initialization
-    const db = getFirestore(app);
     // Reference to the Firestore Data Collection
     const colRef = collection(db, "products");
     const docRef = doc(colRef, productId);
@@ -28,52 +26,7 @@ const Product = () => {
     productFetcher(id)
   }, [])
 
-  // const product = {
-  //   "images": [
-  //     "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/3db2e706-40e9-4ee4-a87c-9b908f4f3db2/NIKE+AIR+MAX+PULSE+ROAM.png",
-  //     "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/4d909366-7b6d-4581-90f0-b53dde469d0f/NIKE+AIR+MAX+PULSE+ROAM.png",
-  //     "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/34e37efb-e6be-4933-853c-8b7f5cb17d74/NIKE+AIR+MAX+PULSE+ROAM.png",
-  //     "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/7cfbc171-dc9d-4163-b71a-b351f590726f/NIKE+AIR+MAX+PULSE+ROAM.png",
-  //     "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/3ce4af74-07cb-4080-95db-61fa9b35a3a8/NIKE+AIR+MAX+PULSE+ROAM.png"
-  //   ],
-  //   "brand": "nike",
-  //   "reviews": [
-  //     {
-  //       "date": "06/10/24",
-  //       "rating": 5,
-  //       "comment": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia fuga tempore hic pariatur mollitia? Vero impedit nesciunt animi accusantium rerum eveniet molestiae. Ipsum soluta eligendi, consequuntur distinctio ratione nesciunt at.",
-  //       "name": "Akku",
-  //       "id": "001r001"
-  //     },
-  //     {
-  //       "comment": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia fuga tempore hic pariatur mollitia? Vero impedit nesciunt animi accusantium rerum eveniet molestiae. Ipsum soluta eligendi, consequuntur distinctio ratione nesciunt at.",
-  //       "date": "21/10/24",
-  //       "name": "Bivash",
-  //       "rating": 5,
-  //       "id": "001r002"
-  //     }
-  //   ],
-  //   "sizes": [
-  //     "3.5",
-  //     "4",
-  //     "4.5",
-  //     "5",
-  //     "5.5",
-  //     "6",
-  //     "6.5",
-  //     "7",
-  //     "7.5"
-  //   ],
-  //   "id": "001",
-  //   "discount": 23,
-  //   "rating": 5,
-  //   "price": "14,270",
-  //   "stock": 5,
-  //   "title": "Nike Air Max Pulse Roam",
-  //   "new": true,
-  //   "category": "Men's Shoes",
-  //   "description": "The Air Max Pulse Roam takes cues from its stylish forerunner and moves it in a more utilitarian direction. This means durability without the discomfort and style without the effort. Visible Air cushioning—revamped from the incredibly plush Air Max 270—delivers the comfort you've come to trust."
-  // }
+  
   const recommendations = [
     {
       "images": [
--- src/components/product/sections/ProductImage.jsx
+++ src/components/product/sections/ProductImage.jsx
@@ -3,26 +3,25 @@ import React, { useState } from 'react';
 
 
 const ProductImage = ({ product }) => {
-    const [ imgIndex, setImgIndex ] = useState(0)
+    const [imgIndex, setImgIndex] = useState(0)
 
     return (
-        <>
-            <section className="product-img-container w-full lg:w-[48%] h-full lg:sticky top-[4rem] flex lg:flex-row flex-col-reverse lg:gap-5">
-                <div className="product-img-map w-full lg:w-[10%] h-[10vh] lg:h-full flex lg:flex-col gap-2">
-                    {
-                        product.images.map((img, index) => (
-                            index !== product.images.length - 1 &&
-                            <img onClick={()=>setImgIndex(index)} className='w-[50px] lg:w-full lg:object-cover object-contain rounded' key={index} src={img} alt="Product Image" />
-                        ))
-                    }
-                </div>
-                <div className="product-img w-full lg:w-[90%]">
-                    {
-                        <img className='w-full h-[45vh] lg:h-[87vh] max-h-[768px] object-cover rounded-md' src={product.images[imgIndex]} alt={`Product Image${imgIndex + 1}`}/>
-                    }
-                </div>
-            </section>
-        </>
+        product &&
+        <section className="product-img-container w-full lg:w-[48%] h-full lg:sticky top-[4rem] flex lg:flex-row flex-col-reverse lg:gap-5">
+            <div className="product-img-map w-full lg:w-[10%] h-[10vh] lg:h-full flex lg:flex-col gap-2">
+                {
+                    product.images.map((img, index) => (
+                        index !== product.images.length - 1 &&
+                        <img onClick={() => setImgIndex(index)} className='w-[50px] lg:w-full lg:object-cover object-contain rounded' key={index} src={img} alt="Product Image" />
+                    ))
+                }
+            </div>
+            <div className="product-img w-full lg:w-[90%]">
+                {
+                    <img className='w-full h-[45vh] lg:h-[87vh] max-h-[768px] object-cover rounded-md' src={product.images[imgIndex]} alt={`Product Image${imgIndex + 1}`} />
+                }
+            </div>
+        </section>
     )
 }
 
